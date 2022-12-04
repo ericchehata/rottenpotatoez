60e98205738f25273a6e261a9652b4a6a8fd1167
ericchehata <eric.chehata@mail.mcgill.ca>
2022-12-04 01:25:08 -0500

    Fixed conflict

--- rotten-potatoez-frontend/src/components/NavBar.js
+++ rotten-potatoez-frontend/src/components/NavBar.js
@@ -11,29 +11,16 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import { AccountCircle, Menu as MenuIcon } from "@mui/icons-material";
+import {
+  AccountCircle,
+  Menu as MenuIcon,
+} from "@mui/icons-material";
 import NavBarStyle from "../styles/NavBarStyle";
 import {ReactComponent as Logo} from "../icons/logo.svg";
 
 function NavBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElAccountMenu, setAnchorElAccountMenu] = React.useState(null);
-<<<<<<< Updated upstream
-=======
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
-  const [isAdmin, setIsAdmin] = React.useState(false);
-
-  React.useEffect(() => {
-    const username = localStorage.getItem("username");
-    if (username) {
-      setIsAuthenticated(true);
-      axios.get(`users/${username}`).then((res) => {
-        setIsAdmin(res.data.admin);
-      });
-    }
-    // eslint-disable-next-line
-  }, []);
->>>>>>> Stashed changes
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -49,14 +36,6 @@ function NavBar() {
 
   const handleAccountMenuClose = () => {
     setAnchorElAccountMenu(null);
-<<<<<<< Updated upstream
-=======
-    if (page.name === "Logout") {
-      localStorage.removeItem("username");
-      setIsAuthenticated(false);
-      setIsAdmin(false);
-    }
->>>>>>> Stashed changes
   };
 
   const pages = [{ name: "Add movie", link: "/add-movie" }];
@@ -68,7 +47,6 @@ function NavBar() {
   ];
 
   return (
-<<<<<<< Updated upstream
       <AppBar position="static" color="primary">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
@@ -111,94 +89,13 @@ function NavBar() {
                 ))}
               </Menu>
             </Box>
-            <ThumbsUpDownIcon
-              sx={NavBarStyle.logo}
-            />
+            <SvgIcon component={Logo} sx={{mt:4, height:"60px", width:"60px"}} viewBox="0 0 100 100" />
             <Typography
               variant="h5"
               noWrap
               component="a"
               href="/"
               sx={NavBarStyle.title}
-=======
-    <AppBar position="static" color="primary">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box sx={NavBarStyle.menuContainer}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={NavBarStyle.menu}
-            >
-              {pages.map((page) => (
-                <MenuItem
-                  component={Link}
-                  to={page.link}
-                  key={page.name}
-                  onClick={handleCloseNavMenu}
-                >
-                  <Typography textAlign="center">{page.name}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <SvgIcon component={Logo} sx={{mt:4, height:"60px", width:"60px"}} viewBox="0 0 100 100" />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/"
-            sx={NavBarStyle.title}
-          >
-            Rotten Potatoez
-          </Typography>
-          <div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleAccountMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElAccountMenu}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElAccountMenu)}
-              onClose={handleAccountMenuClose}
->>>>>>> Stashed changes
             >
               Rotten Potatoez
             </Typography>
